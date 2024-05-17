4b39fd0153d56e1720d87f5e2374a2b465e52f81
TitaniumLab <156667078+TitaniumLab@users.noreply.github.com>
2024-05-17 16:50:01 +0300

    Camera resolurion controller implemented.
    Camera resolurion controller implemented.

--- Assets/Scripts/CameraResolutionController.cs
+++ Assets/Scripts/CameraResolutionController.cs
@@ -2,45 +2,65 @@ using UnityEngine;
 
 namespace CrystalProject
 {
-    //SCRIPT WORK IN PROGRESS------------------
     public class CameraResolutionController : MonoBehaviour
     {
-        private float _defaultViewSize;
         [SerializeField] private Vector2 _minTargetAspectRatio;
         [SerializeField] private Vector2 _maxTargetAspectRatio;
-        [SerializeField] private Vector2 rectCenter = new Vector2(0.5f, 0.5f);
-        [SerializeField] private float _targetWidth;
-        [SerializeField] private float _targetViewSize;
+        [SerializeField] private Vector2 _defaultRectSize = new Vector2(1, 1);
+        [SerializeField] private Vector2 _rectCenter = new Vector2(0.5f, 0.5f);
+        [SerializeField] private float _targetViewSize = 6.5f;
+        private float _previousRatio;
 
         private void Start()
         {
-            float currentRatio = Screen.width / Screen.height;
-            if (currentRatio > _minTargetAspectRatio.x / _minTargetAspectRatio.y)
+            SetCamera();
+        }
+
+        private void FixedUpdate()
+        {
+            float currentRario = (float)Screen.width / (float)Screen.height;
+            if (_previousRatio != currentRario)
             {
-
+                SetCamera();
             }
+        }
 
-            //_defaultViewSize = Camera.main.orthographicSize;
-            //float currentSize = Screen.width / Screen.height * Camera.main.orthographicSize;
-            //if (currentSize < _targetWidth)
-            //{
-            //    float x = _defaultViewSize / Screen.height;
-            //    float horizontal = x * Screen.width;
-            //    Camera.main.orthographicSize = _defaultViewSize * _targetWidth / horizontal;
-            //    Vector2 size = new Vector2(1, 1);
-            //    Camera.main.rect = new Rect(default, size) { center = rectCenter };
-            //}
-            //else
-            //{
-            //    Camera.main.orthographicSize = _targetViewSize;
-            //    Vector2 rectCenter = new Vector2(0.5f, 0.5f);
+        private void SetCamera()
+        {
+            _previousRatio = (float)Screen.width / (float)Screen.height;
+            Debug.Log($"Current screen ratio: {_previousRatio}.");
 
-            //    float yValue = Screen.height / _targetViewSize;
-            //    float xValue = yValue * _targetWidth;
-            //    float scale = xValue / Screen.width;
-            //    Vector2 size = new Vector2(scale, 1);
-            //    Camera.main.rect = new Rect(default, size) { center = rectCenter };
-            //}
+            if (_previousRatio > _minTargetAspectRatio.x / _minTargetAspectRatio.y)
+            {
+                Camera.main.orthographicSize = _targetViewSize;
+                float pixelScale = Screen.height / _minTargetAspectRatio.y;
+                float targetWidth = pixelScale * _minTargetAspectRatio.x;
+                float relativeWidth = targetWidth / Screen.width;
+                Vector2 rectSize = new Vector2(relativeWidth, _defaultRectSize.y);
+                Camera.main.rect = new Rect(default, rectSize) { center = _rectCenter };
+                Debug.Log($"New camera rect: {rectSize}.");
+            }
+            else if (_previousRatio < _maxTargetAspectRatio.x / _maxTargetAspectRatio.y)
+            {
+                SetRelariveCameraViewSize();
+                float pixelScale = Screen.width / _maxTargetAspectRatio.x;
+                float targetHeight = pixelScale * _maxTargetAspectRatio.y;
+                float relativeHeight = targetHeight / Screen.height;
+                Vector2 rectSize = new Vector2(_defaultRectSize.x, relativeHeight);
+                Camera.main.rect = new Rect(default, rectSize) { center = _rectCenter };
+                Debug.Log($"New camera rect: {rectSize}.");
+            }
+            else
+            {
+                SetRelariveCameraViewSize();
+            }
+        }
+
+        private void SetRelariveCameraViewSize()
+        {
+            float relativeViewSize = _targetViewSize * (_maxTargetAspectRatio.y / _maxTargetAspectRatio.x) / (_minTargetAspectRatio.y / _minTargetAspectRatio.x);
+            Camera.main.orthographicSize = relativeViewSize;
+            Camera.main.rect = new Rect(default, _defaultRectSize) { center = _rectCenter };
         }
     }
 }
