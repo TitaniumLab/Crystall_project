43a5f178a58934c4c974061ec625d991fa00e1ae
TitaniumLab <156667078+TitaniumLab@users.noreply.github.com>
2024-05-20 18:25:22 +0300

    WTF im doing

--- Assets/Scripts/Units/UnitData.cs
+++ Assets/Scripts/Units/UnitData.cs
@@ -1,15 +1,19 @@
 using System;
+using UnityEditor;
 using UnityEngine;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 namespace CrystalProject.Units
 {
+    // SCRIPT WORK IN PROGRESS
     [Serializable]
     public class UnitData
     {
         [field: SerializeField] public Unit Unit { get; private set; }
         [field: SerializeField] public int ScoreOnCombine { get; private set; }
         [field: SerializeField] public bool CanBeDropped { get; private set; }
-        [field: SerializeField] public int ScoreToDrop { get; private set; }
+        public int ScoreToDrop/* { get; private set; }*/;
     }
 }
-
