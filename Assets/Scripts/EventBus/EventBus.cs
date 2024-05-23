5f66bec53a06f1f7be904531002b3dab7831c544
TitaniumLab <156667078+TitaniumLab@users.noreply.github.com>
2024-05-23 22:40:03 +0300

    EventBus implemented

--- Assets/Scripts/EventBus/EventBus.cs
+++ Assets/Scripts/EventBus/EventBus.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace CrystalProject.EventBus
 {
@@ -6,10 +8,35 @@ namespace CrystalProject.EventBus
     {
         private Dictionary<string, List<object>> _siganalCallbacks = new Dictionary<string, List<object>>();
 
-        private void sadf()
+        public void Subscribe<T>(Action<T> callback)
         {
-            
+            string key = typeof(T).Name;
+            if (_siganalCallbacks.ContainsKey(key))
+                _siganalCallbacks[key].Add(callback);
+            else
+                _siganalCallbacks.Add(key, new List<object>() { callback });
         }
 
+        public void Unsubscribe<T>(Action<T> callback)
+        {
+            string key = typeof(T).Name;
+            if (_siganalCallbacks.ContainsKey(key))
+                _siganalCallbacks[key].Remove(callback);
+            else
+                Debug.LogError($"Signal type of {key} not exist");
+        }
+
+        public void Invoke<T>(T signal)
+        {
+            string key = typeof(T).Name;
+            if (_siganalCallbacks.ContainsKey(key))
+            {
+                foreach (var item in _siganalCallbacks[key])
+                {
+                    var callback = item as Action<T>;
+                    callback?.Invoke(signal);
+                }
+            }
+        }
     }
 }
