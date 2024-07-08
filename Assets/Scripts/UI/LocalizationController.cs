b90251a2e802c9c60f4979a44f7a9df2ffeffaf6
TitaniumLab <156667078+TitaniumLab@users.noreply.github.com>
2024-07-08 18:05:27 +0300

    Refactoring

--- Assets/Scripts/UI/LocalizationController.cs
+++ Assets/Scripts/UI/LocalizationController.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
 using YG;
 
 namespace CrystalProject.UI
@@ -9,6 +10,8 @@ namespace CrystalProject.UI
     {
         private static LocalizationController s_instance;
         [SerializeField] private int _currentLocIndex = 0;
+        [SerializeField] private Sprite[] _languageSprite;
+        [SerializeField] private Image _languageImage;
         [SerializeField] private TMP_Dropdown _localizationDropDown;
 
         private void Awake()
@@ -16,7 +19,7 @@ namespace CrystalProject.UI
             if (s_instance is not null)
             {
                 Debug.Log(s_instance);
-                s_instance.SetInstance(_localizationDropDown);
+                s_instance.SetInstance(_localizationDropDown, _languageImage, _languageSprite);
                 Destroy(this);
                 return;
             }
@@ -33,22 +36,26 @@ namespace CrystalProject.UI
             if (_localizationDropDown.value != _currentLocIndex)
             {
                 _localizationDropDown.value = _currentLocIndex;
-                ChangeLanguage(_currentLocIndex);
             }
         }
 
-        private void SetInstance(TMP_Dropdown dropdown)
+
+        private void SetInstance(TMP_Dropdown dropdown, Image image, Sprite[] flagSprites)
         {
             _localizationDropDown.onValueChanged.RemoveListener(ChangeLanguage);
             _localizationDropDown = dropdown;
-            _localizationDropDown.value = _currentLocIndex;
+            _languageSprite = flagSprites;
+            _languageImage = image;
             _localizationDropDown.onValueChanged.AddListener(ChangeLanguage);
+            _localizationDropDown.value = _currentLocIndex;
         }
 
         private void ChangeLanguage(int languageIndex)
         {
             _currentLocIndex = languageIndex;
             LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[languageIndex];
+            _languageImage.sprite = _languageSprite[languageIndex];
+            Debug.Log("Language changed.");
         }
 
         public void SaveLanguage()
